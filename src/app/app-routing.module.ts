fb2dbdafd5f22396c20936cefaec17bafe01e071
Nikhil Vyas <nikhilvays771@gmail.com>
2020-07-01 12:21:57 +0530

    file added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BillNewComponent } from './bill-new/bill-new.component';
+import { SearchBillComponent } from './search-bill/search-bill.component';
+
+
+const routes: Routes = [
+  {path:'newBill',component:BillNewComponent},
+  {path:'searchBill',component:SearchBillComponent},
+
+  {path:'',redirectTo:'newBill',pathMatch:'full'},
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
